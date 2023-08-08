7be85868b7209022cc8cb10f58a7c386e58cd318
samyz3n <141714224+samyz3n@users.noreply.github.com>
2023-08-08 13:10:44 +0530

    Update EnvironmentSetup.py

--- EnvironmentSetup.py
+++ EnvironmentSetup.py
@@ -3,7 +3,7 @@ import torch
 # setting up device agnostics
 class EnvironmentSetup():
     def __init__(self):
-        !pip install nni torch torchvision pytorch-lightning
+        pass
     
     def set_device_agnostics():
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
